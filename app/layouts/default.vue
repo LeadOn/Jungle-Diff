c45725d87965d53b87a5e7403024f4b486eeef92
Valentin Virot <virot.valentin@gmail.com>
2026-08-12 15:08:43 +0200

    Fixed design issues

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,16 +3,16 @@
     <!-- Subtle glow behind the page -->
     <div class="pointer-events-none absolute inset-0 z-0 overflow-hidden flex justify-center">
       <div class="w-full max-w-[1200px] h-full relative">
-         <div class="absolute top-[10%] right-[10%] w-[600px] h-[600px] bg-white/40 rounded-full blur-[100px]"></div>
+         <div class="absolute top-[10%] right-[10%] w-[600px] h-[600px] bg-(--color-glow) rounded-full blur-[100px]"></div>
       </div>
     </div>
 
     <!-- Content Wrapper -->
     <div class="relative z-10 flex flex-col flex-grow">
       <AppHeader />
-      
+
       <!-- Main Content Container -->
-      <main class="flex-grow w-full max-w-[1200px] mx-auto px-6 py-12">
+      <main class="flex-grow w-full max-w-[1200px] mx-auto px-6 pt-6 pb-16 md:py-12">
         <slot />
       </main>
     </div>
